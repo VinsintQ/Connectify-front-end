9eacb25ff22d7432d334a75ce6906159e8efffeb
ali-alaalem <Meemo2002a@gmail.com>
2024-09-06 17:28:44 +0300

    Adding the followers services

---
+++ src/services/followers.js
@@ -0,0 +1,44 @@
+const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/follower`;
+
+const index = async (userId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${userId}`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    });
+    return res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const create = async (username, userId) => {
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, userId }),
+  };
+  const res = await fetch(`${BASE_URL}/add-friend`, options);
+
+  return res.json();
+};
+
+const show = async (userId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${userId}`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    });
+
+    return res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default {
+  index,
+  create,
+  show,
+};
