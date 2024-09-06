4a5fe5acd10a1081bc1ea604ac616f9f41cb8a66
Mueen <hejiy2002@gmail.com>
2024-09-07 01:21:11 +0300

    creating postservice and a function to retrieve all post  , showing all posts in home page

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,13 +1,34 @@
 // src/components/Dashboard.jsx
 
+import { useState,useEffect } from "react";
+import postService from "../../services/postService";
+
 const Dashboard = ({ user }) => {
+ const [allpost,setallpost] =  useState([])
+
+ useEffect(() => {
+  if (user && user._id) {
+    const fetchposts = async () => {
+      try {
+        const userData = await postService.allposts();
+        setallpost(userData);
+      } catch (error) {
+        console.error("Failed to fetch user data:", error);
+      }
+    };
+    fetchposts();
+  }
+}, [user]);
+
     return (
       <main>
         <h1>Welcome, {user.username}</h1>
-        <p>
-          This is the dashboard page where you, and only you, can see a dashboard
-          of all of your things.
-        </p>
+        {allpost.map((post) => (
+          <div key={post._id}>
+            <h3>{post.content}</h3>
+            <p></p>
+          </div>
+        ))}
       </main>
     );
   };
