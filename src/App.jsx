72693ce2e56c622dd581f6af82f52101c4663978
Mueen <hejiy2002@gmail.com>
2024-09-10 20:18:46 +0300

    delete import sign in and sign up from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ import ChatPage from "./components/ChatPage/ChatPage";
 import NavBar from "./components/NavBar/NavBar";
 import Landing from "./components/Landing/Landing";
 import Dashboard from "./components/Dashboard/Dashboard";
-import SignupForm from "./components/SignupForm/SignupForm";
-import SigninForm from "./components/SigninForm/SigninForm";
+
 import Search from "./components/Search/Search";
 import Profile from "./components/Profile/Profile";
 import MyCompanies from "./components/myCompanies/myCompanies";
