908df0d6ab2d0fa859205444adc9e2fb1c798102
Mueen <hejiy2002@gmail.com>
2024-09-11 10:08:21 +0300

    adding update function to user Services

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -23,7 +23,24 @@ const show = async (userId) => {
   }
 };
 
+const update = async (userId, formData) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${userId}`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    return res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
 export default {
   index,
   show,
+  update,
 };
