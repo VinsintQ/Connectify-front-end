ea87caa64af54fed1ccb8fa0bb01dd84afe0b2b7
Mueen <hejiy2002@gmail.com>
2024-11-25 20:18:14 +0300

    style

--- src/components/myCompanies/CompnayDetails.jsx
+++ src/components/myCompanies/CompnayDetails.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
+import "./CompanyDetails.css";
 
-;
 import companyService from "../../services/companyService";
 
 const CompanyDetails = ({ user }) => {
@@ -38,19 +38,23 @@ const CompanyDetails = ({ user }) => {
   }
 
   return (
-    <div>
+    <div className="company-details-container">
       <h3>Company Details</h3>
       <p>Name : {company.name}</p>
-      <p>Indusrty : {company.industry}</p>
+      <p>Industry : {company.industry}</p>
       <p>About : {company.about}</p>
-
+  
       <div>
-      <Link to={`/Mycompany/company/${company._id}/addJob`}><h2>Post a new job</h2></Link> 
-      <Link to={`/Mycompany/company/${company._id}/jobs`}><h2>View all jobs</h2></Link>
+        <Link to={`/Mycompany/company/${company._id}/addJob`}>
+          <h2 className="bt">Post a new job</h2>
+        </Link> 
+        <Link to={`/Mycompany/company/${company._id}/jobs`}>
+          <h2 className="bt">View all jobs</h2>
+        </Link>
       </div>
-      
     </div>
   );
+  
 };
 
 export default CompanyDetails;
