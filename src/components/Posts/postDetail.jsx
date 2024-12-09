762afd83c97fb4365a0d0911c79e63387f5813b3
Mueen <hejiy2002@gmail.com>
2024-12-09 14:26:00 +0300

    FIXING DELETE POST FUNCTION

--- src/components/Posts/postDetail.jsx
+++ src/components/Posts/postDetail.jsx
@@ -23,7 +23,7 @@ const PostDetails = ({ user }) => {
 
   const handleConfirmDeletePost = async () => {
     await postService.deleter(userId, postId);
-    navigate("/posts");
+    navigate("/post");
   };
 
   const deleteComment = (commentId) => {
